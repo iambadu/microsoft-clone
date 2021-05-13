4a67287b43a2c3bca95ef5b0de466532d033eb56
iambadu <baduplus@gmail.com>
2021-05-13 08:11:59 +0200

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore itaque, molestias quam quasi impedit esse, sed unde enim corrupti non maxime eius nisi facilis nobis ducimus laudantium odit vel nemo.
+    </>
   );
 }
 
